d0f9081a82805750704b14ccec01969ed003f38d
Brandon Huynh <brandon.huynh@live.com>
2021-09-21 20:57:25 -0700

    Completed

--- src/edu/calpoly/csc305/debugging/Thesaurus.java
+++ src/edu/calpoly/csc305/debugging/Thesaurus.java
@@ -12,8 +12,19 @@ public class Thesaurus {
     synonyms = new HashMap<>();
   }
 
+  /** adds alternative synonyms for word in Thesaurus.
+   *  creates new key value if word is not present in Thesaurus
+   *
+   * @param word the root word
+   * @param alternatives Array of strings that are alternative words to word.
+   */
   public void addAlternatives(String word, String ... alternatives) {
-    synonyms.get(word).addAll(Arrays.asList(alternatives));
+
+    if (synonyms.containsKey(word)) {
+      synonyms.get(word).addAll(Arrays.asList(alternatives));
+    } else {
+      synonyms.put(word, Arrays.asList(alternatives));
+    }
   }
 
   public List<String> alternatives(String word) {
